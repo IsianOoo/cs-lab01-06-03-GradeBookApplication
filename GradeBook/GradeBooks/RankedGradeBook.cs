051e41c5e4e4f29e4bde81f4122f41b986b668a9
Krystian Mączeński <Krystian.Maczenski@microsoft.wsei.edu.pl>
2022-04-04 16:10:16 +0200

    Override Get

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -12,7 +12,7 @@ namespace GradeBook.GradeBooks
         }
         public override char GetLetterGrade(double averageGrade)
         {
-            if(StudentCount()>5)
+            if(StudentCount()<5)
             {
                 throw new InvalidOperationException("Not enough students.");
 
@@ -28,6 +28,13 @@ namespace GradeBook.GradeBooks
                     return 'D';
                 else return 'F';
             }
+        }
+        public override void CalculateStatistics()
+        {
+            if (StudentCount() < 5)
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+            else if (StudentCount() >= 5)
+                base.CalculateStatistics();
         }
     }
 }
